6cedee2319bbbe7f3c8eac055374e73c02d1482d
Muhammad Saim <160173114+MuhammadSaim32@users.noreply.github.com>
2025-10-23 23:59:37 +0500

    Update corn_loger.py

--- corn_loger.py
+++ corn_loger.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+LOG_FILE = "/data/logs.txt"
 
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-message = f"[{now}] Hello from Railway cron job!\n"
+message = f"[{now}] Cron ran\n"
+
+with open(LOG_FILE, "a") as f:
+    f.write(message)
 
 print(message.strip())
-
-with open("log.txt", "a") as f:
-    f.write(message)
